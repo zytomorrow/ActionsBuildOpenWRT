d0cf188148926960eb9884f497b7f4a0c448ba7d
zytomorrow <z794672847@gmail.com>
2025-02-13 22:54:18 +0800

    Update diy-part1.sh

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -4,4 +4,4 @@
 echo "src-git openwrt_kiddin9 https://github.com/kiddin9/kwrt-packages.git" >> feeds.conf.default
 
 # luci-app-mihomo
-echo "src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main" >> "feeds.conf.default"
+# echo "src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main" >> "feeds.conf.default"
